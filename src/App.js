baed1e97fd34e0b48187a33c7df7a6116510866c
Siyu <nimagangfour@gmail.com>
2020-02-07 19:48:58 +1100

    Switch + Router

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import { Route, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Landing from "./components/Pages/Landing";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
+import FAQ from "./components/Pages/FAQ";
+import News from "./components/Pages/News";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/faq" component={FAQ} />
+        <Route exact path="/news" component={News} />
+      </Switch>
       <header className="App-header">
-        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,19 +18,23 @@ class Header extends Component {
         <Navbar bg="light" expand="lg">
           {" "}
           <Navbar.Brand href="#home">
-            <img
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              src={Logo_White}
-              alt="logo"
-            ></img>
+            <NavLink to="/">
+              <img
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                src={Logo_White}
+                alt="logo"
+              ></img>
+            </NavLink>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <NavLink to="/about">About</NavLink>
+              <NavLink href="/contact">Contact</NavLink>
+              <NavLink href="/news">News</NavLink>
+              <NavLink href="/faq">FAQs</NavLink>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
