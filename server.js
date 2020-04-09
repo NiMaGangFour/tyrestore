eb4ddbb6edbdea887f144024a72c411c2647cbd2
Siyu <nimagangfour@gmail.com>
2020-04-09 22:43:14 +1000

    Read output to browser

--- server.js
+++ server.js
@@ -93,6 +93,57 @@ app.get("/i", (req, res) => {
 //@desc uploads file to DB
 app.post("/upload", upload.single("file"), (req, res) => {
   res.json({ file: req.file });
+  // res.redirect("/");
+});
+
+//@route GET / files
+//@desc Display all files in JSON
+app.get("/files", (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    //Check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: "No file exist",
+      });
+    }
+
+    //Files exist
+    return res.json(files);
+  });
+});
+
+//@route GET /files/:filename
+//@desc Display single file object
+app.get("/files/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    //Check if files
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exist",
+      });
+    }
+    //Files exist
+    return res.json(file);
+  });
+});
+
+//@route GET /image/:filename
+//@desc Display image
+app.get("/image/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    //Check if files
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exist",
+      });
+    }
+    //Check if image
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      //Read output to browser
+    }
+    //Files exist
+    return res.json(file);
+  });
 });
 
 // create a GET route
